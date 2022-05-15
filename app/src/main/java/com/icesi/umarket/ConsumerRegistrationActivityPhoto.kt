34ff5aa16169b6814b194ad891158c7ed0d7c697
ValeArias07 <1000255081@u.icesi.edu.co>
2022-05-14 19:14:13 -0500

    Skeleton of pages

---
+++ app/src/main/java/com/icesi/umarket/ConsumerRegistrationActivityPhoto.kt
@@ -0,0 +1,11 @@
+package com.icesi.umarket
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConsumerRegistrationActivityPhoto : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_consumer_registration_photo)
+    }
+}

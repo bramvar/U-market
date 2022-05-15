e180cb7abd57b4d80bacd856f102585ce5462fde
J. Var <bramf0@protonmail.com>
2022-05-14 23:50:11 -0500

    create Seller data class

---
+++ app/src/main/java/com/icesi/umarket/Seller.kt
@@ -0,0 +1,10 @@
+package com.icesi.umarket
+
+data class Seller(
+    val marketName: String,
+    val sellerName: String,
+    val email: String,
+    val password: String,
+    val phone: String,
+    val marketDescription: String
+)

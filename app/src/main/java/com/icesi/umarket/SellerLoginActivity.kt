92b20dfa31cfd7c4d4eddc2370a79ff7e1d67cbd
J. Var <bramf0@protonmail.com>
2022-05-14 21:50:02 -0500

    add navigation between seller registration screens

--- app/src/main/java/com/icesi/umarket/SellerLoginActivity.kt
+++ app/src/main/java/com/icesi/umarket/SellerLoginActivity.kt
@@ -1,11 +1,25 @@
 package com.icesi.umarket
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.icesi.umarket.databinding.ActivitySellerLoginBinding
 
 class SellerLoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySellerLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_seller_login)
+
+        binding = ActivitySellerLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signUpHyperLink.setOnClickListener {
+            startActivity(Intent(this,SellerSignupActivity::class.java))
+        }
+        binding.sellerLoginGoBackBtn.setOnClickListener {
+            finish()
+        }
     }
 }
--- app/src/main/java/com/icesi/umarket/SellerSignupActivity.kt
+++ app/src/main/java/com/icesi/umarket/SellerSignupActivity.kt
@@ -2,10 +2,23 @@ package com.icesi.umarket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.icesi.umarket.databinding.ActivitySellerSignupBinding
 
 class SellerSignupActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySellerSignupBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_seller_signup)
+
+        binding = ActivitySellerSignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.sellerLogInHyperLink.setOnClickListener {
+            finish()
+        }
+        binding.sellerSignupGoBackBtn.setOnClickListener {
+            finish()
+        }
     }
 }

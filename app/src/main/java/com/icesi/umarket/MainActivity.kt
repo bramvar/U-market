76baafc87ca40fd2f971123e5a15cf936e79bfdc
Valentina Arias <valeariasparra2001@gmail.com>
2022-05-15 19:10:09 -0500

    fix MainActivity error

--- app/src/main/java/com/icesi/umarket/MainActivity.kt
+++ app/src/main/java/com/icesi/umarket/MainActivity.kt
@@ -2,15 +2,12 @@ package com.icesi.umarket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.icesi.umarket.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_consumer_registration_photo)
+        setContentView(R.layout.activity_main)
     }
 
 }

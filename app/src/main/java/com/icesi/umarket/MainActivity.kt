9435a7b283ff4a2657ca7ac3fc9891b616ab1009
J. Var <bramf0@protonmail.com>
2022-05-15 20:14:54 -0500

    add bar navigation menu to seller home screen

--- app/src/main/java/com/icesi/umarket/MainActivity.kt
+++ app/src/main/java/com/icesi/umarket/MainActivity.kt
@@ -3,8 +3,11 @@ package com.icesi.umarket
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.icesi.umarket.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

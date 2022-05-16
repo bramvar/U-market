41a93687ed6e736a366591a9f0e57f06d39049cf
J. Var <bramf0@protonmail.com>
2022-05-16 15:47:26 -0500

    add binding to current fragments and set up navigation between them

--- app/src/main/java/com/icesi/umarket/NewProductFragment.kt
+++ app/src/main/java/com/icesi/umarket/NewProductFragment.kt
@@ -5,15 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.icesi.umarket.databinding.FragmentNewProductBinding
 
 class NewProductFragment : Fragment() {
+
+    private var _binding: FragmentNewProductBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_product, container, false)
+
+        _binding = FragmentNewProductBinding.inflate(inflater, container, false)
+
+        binding.postNewProductBtn.setOnClickListener {
+
+        }
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
--- app/src/main/java/com/icesi/umarket/SellerMainOverviewFragment.kt
+++ app/src/main/java/com/icesi/umarket/SellerMainOverviewFragment.kt
@@ -5,15 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.icesi.umarket.databinding.FragmentSellerMainOverviewBinding
 
 class SellerMainOverviewFragment : Fragment() {
+
+    private var _binding: FragmentSellerMainOverviewBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_seller_main_overview, container, false)
+        _binding = FragmentSellerMainOverviewBinding.inflate(inflater,container,false)
+
+
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
     }
 
     companion object {

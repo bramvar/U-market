0dbee690a9904c6a3d34a99154ae0feebf1cb7f2
NatalyAM11 <60659258+NatalyAM11@users.noreply.github.com>
2022-05-18 17:20:21 -0500

    profile consumer

---
+++ app/src/main/java/com/icesi/umarket/ConsumerProfile.kt
@@ -0,0 +1,11 @@
+package com.icesi.umarket
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConsumerProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_consumer_profile)
+    }
+}

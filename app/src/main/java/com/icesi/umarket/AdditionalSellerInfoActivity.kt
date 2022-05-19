3ceb8b9794d3be8f032cc8434a887eaacf2e142d
J. Var <bramf0@protonmail.com>
2022-05-19 01:59:49 -0500

    implement gallery launcher to pick image. Set up firebase storage to upload selected image. Add firebase storage dependency

--- app/src/main/java/com/icesi/umarket/AdditionalSellerInfoActivity.kt
+++ app/src/main/java/com/icesi/umarket/AdditionalSellerInfoActivity.kt
@@ -1,25 +1,34 @@
 package com.icesi.umarket
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import com.icesi.umarket.databinding.ActivityAdditionalSellerInfoBinding
+import com.icesi.umarket.model.Market
+import com.icesi.umarket.model.Product
+import java.util.*
 
 class AdditionalSellerInfoActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAdditionalSellerInfoBinding
+    private lateinit var imageUri: Uri
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityAdditionalSellerInfoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val galleryLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult(),::onGalleryResult)
+
         val id =intent.getStringExtra("ID")
-        val marketName = intent.getStringExtra("MARKET_NAME")
+        val phone = intent.getStringExtra("PHONE")
         val sellerName = intent.getStringExtra("SELLER_NAME")
         val email = intent.getStringExtra("EMAIL")
         val password = intent.getStringExtra("PASSWORD")
@@ -27,18 +36,34 @@ class AdditionalSellerInfoActivity : AppCompatActivity() {
         binding.sellerSignupBtn.setOnClickListener {
             val i = Intent(this,SellerLoginActivity::class.java)
 
-            val sellerPhone = binding.sellerInfoPhoneTextField.text.toString()
+            val marketID = UUID.randomUUID().toString()
+            val marketName = binding.signUpMarketNameTextField.text.toString()
             val marketDescription = binding.sellerInfoDescriptionTextField.text.toString()
+            val fileName = UUID.randomUUID().toString()
 
-            if(verifyBlankAdditionalInfoFields(sellerPhone,marketDescription)){
-                val seller = Seller(id!!,marketName!!,sellerName!!,email!!,password!!,sellerPhone,marketDescription,"Seller")
+            if(verifyBlankAdditionalInfoFields(marketName,marketDescription)){
+                val seller = Seller(id!!,sellerName!!,email!!,password!!,phone!!,"Seller")
+
+                val market = Market(marketID,id,marketName,marketDescription,fileName)
+
                 Firebase.firestore.collection("users").document(id).set(seller).addOnSuccessListener {
-                    Toast.makeText(this.baseContext,"Usuario registrado. Inicie sesión", Toast.LENGTH_LONG).show()
-                    startActivity(i)
+                    Firebase.firestore.collection("markets").document(marketID).set(market).addOnSuccessListener {
+                        Firebase.storage.getReference().child("market-image-profile").child(fileName).putFile(imageUri)
+                        Toast.makeText(this.baseContext,"Usuario registrado. Inicie sesión", Toast.LENGTH_LONG).show()
+                        startActivity(i)
+                    }.addOnFailureListener {
+                        Toast.makeText(this.baseContext,it.message, Toast.LENGTH_LONG).show()
+                    }
                 }
             }else {
                 Toast.makeText(this.baseContext,"Faltan campos por diligenciar", Toast.LENGTH_LONG).show()
             }
+        }
+
+        binding.newMarketImage.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+            galleryLauncher.launch(intent)
         }
 
         binding.sellerAdditionalInfoBackBtn.setOnClickListener {
@@ -46,10 +71,19 @@ class AdditionalSellerInfoActivity : AppCompatActivity() {
         }
     }
 
+    private fun onGalleryResult(activityResult: ActivityResult){
+        if(activityResult.resultCode == RESULT_OK){
+            imageUri= activityResult.data?.data!!
+            binding.newMarketImage.setImageURI(imageUri)
+
+            //image upload
+        }
+    }
+
     private fun verifyBlankAdditionalInfoFields(
-        sellerPhone: String,
+        marketName: String,
         marketDescription: String,
     ): Boolean {
-        return sellerPhone.isNotBlank() && marketDescription.isNotBlank()
+        return marketName.isNotBlank() && marketDescription.isNotBlank()
     }
 }

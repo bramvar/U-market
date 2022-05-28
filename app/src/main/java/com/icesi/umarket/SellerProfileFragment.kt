07dda5db61cbaaed4c0eb5404478422cb2514263
J. Var <bramf0@protonmail.com>
2022-05-28 13:18:35 -0500

    Refactor: convert seller profile activity to a fragment

---
+++ app/src/main/java/com/icesi/umarket/SellerProfileFragment.kt
@@ -0,0 +1,24 @@
+package com.icesi.umarket
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.icesi.umarket.databinding.FragmentSellerProfileBinding
+
+class SellerProfileFragment : Fragment() {
+
+    private var _binding: FragmentSellerProfileBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentSellerProfileBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+}

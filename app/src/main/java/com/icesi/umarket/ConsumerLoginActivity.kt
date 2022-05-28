18d7e8ebb87eec6baee539699f8c1ccfa0c9a8af
Valentina Arias <valeariasparra2001@gmail.com>
2022-05-28 14:13:30 -0500

    modify consumer and seller login. Also, it contains the consumer home with their recyclerview working. Changes made by Juan Hernandez

--- app/src/main/java/com/icesi/umarket/ConsumerLoginActivity.kt
+++ app/src/main/java/com/icesi/umarket/ConsumerLoginActivity.kt
@@ -7,16 +7,32 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import com.icesi.umarket.databinding.ActivitySellerLoginBinding
+import com.google.gson.Gson
+
+import com.icesi.umarket.databinding.ActivityConsumerLoginBinding
 import com.icesi.umarket.model.User
 
+
 class ConsumerLoginActivity : AppCompatActivity() {
-    private lateinit var binding: ActivitySellerLoginBinding
+    private lateinit var binding: ActivityConsumerLoginBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySellerLoginBinding.inflate(layoutInflater)
+        binding = ActivityConsumerLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.signUpHyperLink.setOnClickListener {
+            var userType = intent.extras?.getString("userType","")
+            if(userType=="consumer"){
+                startActivity(Intent(this,ConsumerSignupActivity::class.java))
+            }else{
+                startActivity(Intent(this,SellerSignupActivity::class.java))
+            }
+        }
+        binding.sellerLoginGoBackBtn.setOnClickListener {
+            finish()
+        }
 
 
         binding.loginBtn.setOnClickListener {
@@ -31,7 +47,7 @@ class ConsumerLoginActivity : AppCompatActivity() {
                         .addOnSuccessListener {
                             val user = it.toObject(User::class.java)
 
-                            //saveUser(user!!)
+                            saveUser(user!!)
                             startActivity(Intent(this, ConsumerHomeActivity::class.java))
                             finish()
                         }.addOnFailureListener {
@@ -40,6 +56,14 @@ class ConsumerLoginActivity : AppCompatActivity() {
                 }.addOnFailureListener {
                     Toast.makeText(this.baseContext, it.message, Toast.LENGTH_LONG).show()
                 }
+
         }
     }
+    private fun saveUser(user: User){
+        val sp = getSharedPreferences("u-market", MODE_PRIVATE)
+        val json = Gson().toJson(user)
+        sp.edit().putString("user",json).apply()
+
+    }
 }
+
---
+++ app/src/main/java/com/icesi/umarket/ConsumerMainOverviewFragment.kt
@@ -0,0 +1,73 @@
+package com.icesi.umarket
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
+import com.icesi.umarket.databinding.FragmentConsumerMainOverviewBinding
+import com.icesi.umarket.model.Market
+import com.icesi.umarket.model.Product
+import com.icesi.umarket.model.ProductAdapter
+
+
+class ConsumerMainOverviewFragment : Fragment() {
+
+    private var _binding: FragmentConsumerMainOverviewBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var user: Seller
+
+    //STATE
+    private val adapter = ProductAdapter()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentConsumerMainOverviewBinding.inflate(inflater,container,false)
+        //user = loadUser()!!
+
+        getMarket()
+
+        val productRecycler = binding.recyclerView
+        productRecycler.setHasFixedSize(true)
+        productRecycler.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
+        productRecycler.adapter = adapter
+
+        return binding.root
+        //return inflater.inflate(R.layout.fragment_consumer_main_overview, container, false)
+    }
+
+    fun getMarket(){
+        Firebase.firestore.collection("markets").document("2c63f174-0beb-446f-b7c8-e4e41ad1e927").get()
+            .addOnSuccessListener {
+                val currentMarket = it.toObject(Market::class.java)
+                val marketName = currentMarket?.marketName
+                val marketImage = currentMarket?.imageID
+                getProducts("2c63f174-0beb-446f-b7c8-e4e41ad1e927")
+            }
+    }
+
+    fun getProducts(marketID: String){
+        Firebase.firestore.collection("markets").document("2c63f174-0beb-446f-b7c8-e4e41ad1e927").collection("products").get()
+            .addOnCompleteListener { product ->
+                adapter.clear()
+                for(doc in product.result!!) {
+                    val prod = doc.toObject(Product::class.java)
+                    adapter.addProduct(prod)
+                }
+            }
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = ConsumerMainOverviewFragment()
+
+    }
+}

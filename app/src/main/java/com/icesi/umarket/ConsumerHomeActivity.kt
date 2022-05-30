527acae557a0140fcdd779abd801370b4ca9b523
Valentina Arias <valeariasparra2001@gmail.com>
2022-05-29 22:01:32 -0500

    fix the ConsumerProfileFragment bug and add some changes in the ConsumerHomeActivity

--- app/src/main/java/com/icesi/umarket/ConsumerHomeActivity.kt
+++ app/src/main/java/com/icesi/umarket/ConsumerHomeActivity.kt
@@ -4,30 +4,37 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.icesi.umarket.model.User
 
 class ConsumerHomeActivity : AppCompatActivity() {
 
     private lateinit var menuConsumer:BottomNavigationView
     private lateinit var consumerMainOverviewFragment: ConsumerMainOverviewFragment
-    private lateinit var consumerProfileFragment: ConsumerProfileTest
+    private lateinit var consumerProfileFragment: ConsumerProfileFragment
+
+    private lateinit var currentUser: User
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_consumer_home)
 
+        //Extras
+
         consumerMainOverviewFragment = ConsumerMainOverviewFragment.newInstance()
-        consumerProfileFragment = ConsumerProfileTest.newInstance()
+        consumerProfileFragment = ConsumerProfileFragment.newInstance()
+        consumerProfileFragment.currentUser = currentUser
 
         menuConsumer = findViewById(R.id.menuConsumer)
         showFragment(consumerMainOverviewFragment)
 
-        menuConsumer.setOnItemReselectedListener { menuItem->
-            if(menuItem.itemId == R.id.homeItem){
-                showFragment(consumerMainOverviewFragment)
-            }else if (menuItem.itemId == R.id.profileItem){
-                showFragment(consumerProfileFragment)
-            }else if (menuItem.itemId == R.id.ordersItem){
-                showFragment(consumerMainOverviewFragment)
+        menuConsumer.setOnItemSelectedListener { menuItem->
+            when(menuItem.itemId){
+                R.id.homeItem -> showFragment(consumerMainOverviewFragment)
+                R.id.profileItem -> showFragment(consumerProfileFragment)
+                R.id.ordersItem -> showFragment(consumerMainOverviewFragment)
             }
             true
         }

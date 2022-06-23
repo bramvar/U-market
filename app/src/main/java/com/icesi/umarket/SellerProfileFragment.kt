d4e20cf4c5b2849baf82a61cd098b348baa97d64
Valentina Arias <valeariasparra2001@gmail.com>
2022-06-23 18:07:01 -0500

    fix merge

--- app/src/main/java/com/icesi/umarket/SellerProfileFragment.kt
+++ app/src/main/java/com/icesi/umarket/SellerProfileFragment.kt
@@ -7,24 +7,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.firebase.auth.ktx.auth
-import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.google.gson.Gson
-import com.icesi.umarket.databinding.FragmentSellerOrderOverviewBinding
-import com.icesi.umarket.databinding.FragmentSellerProfileBinding
-import com.icesi.umarket.model.Market
 import com.icesi.umarket.model.Seller
 import com.icesi.umarket.util.Util
 import com.google.firebase.storage.ktx.storage
-import com.google.gson.Gson
 import com.icesi.umarket.databinding.FragmentSellerProfileBinding
 import com.icesi.umarket.model.Market
 import com.icesi.umarket.model.Product
 
 class SellerProfileFragment : Fragment() {
 
-    private var _binding: FragmentSellerProfileBinding? = null
+    private lateinit var _binding: FragmentSellerProfileBinding
     private val binding get() = _binding!!
     private lateinit var market: Market
     private lateinit var seller: Seller
@@ -73,27 +69,6 @@ class SellerProfileFragment : Fragment() {
         @JvmStatic
         fun newInstance() = SellerProfileFragment()
 
-    }
-
-    fun downloadMarketProfileImage(imageID: String?){
-        if(imageID == null) return
-
-        Firebase.storage.reference.child("market-image-profile").child(imageID).downloadUrl
-            .addOnSuccessListener{
-                Glide.with(binding.MarketImageProfile).load(it).into(binding.MarketImageProfile)
-            }
-    }
-
-    fun getProducts(marketID: String){
-        Firebase.firestore.collection("markets").document(marketID).collection("products").get()
-            .addOnCompleteListener { product ->
-                adapter.clear()
-                for(doc in product.result!!){
-                    val prod =doc.toObject(Product::class.java)
-                    adapter.addProduct(prod)
-                }
-
-            }
     }
 
 }

3c0bca901218251cf68356c4f7c580f9abdd6cf9
Valentina Arias <valeariasparra2001@gmail.com>
2022-06-26 10:51:45 -0500

    add settings to create a dialogfragment to edit a product

--- app/src/main/java/com/icesi/umarket/EditProductDialogFragment.kt
+++ app/src/main/java/com/icesi/umarket/EditProductDialogFragment.kt
@@ -4,13 +4,14 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.DialogFragment
+import com.icesi.umarket.databinding.ActivityAdditionalConsumerInfoBinding.inflate
 import com.icesi.umarket.databinding.FragmentEditProductDialogBinding
-import com.icesi.umarket.databinding.FragmentProductSellerBinding
 import com.icesi.umarket.model.Product
 import com.icesi.umarket.seller.SellerMainOverviewFragment
 
@@ -21,15 +22,31 @@ class EditProductDialogFragment : DialogFragment() {
     private lateinit var product : Product
     lateinit var onProductSellerObserver: SellerMainOverviewFragment.OnProductsOnSellerObserver
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view: View = inflater.inflate(R.layout.fragment_edit_product_dialog, null)
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        _binding = FragmentEditProductDialogBinding.inflate(inflater, container, false)
+
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+
+    fun loadData(){
+        var price = Integer.parseInt(product.price.toString())
+        Log.e("Product in Dialog",product.name +" - "+product.description+" - "+product.price )
+        //binding.priceProductEditSeller.setText(price)
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        _binding = FragmentEditProductDialogBinding.inflate(LayoutInflater.from(context))
+
+        var dialog = AlertDialog.Builder(requireContext())
+            .setView(_binding.root)
+            .create()
+
         loadData()
 
-        binding.acceptChangesButton.setOnClickListener {
+        _binding.acceptChangesButton.setOnClickListener {
             var name= binding.nameProductEditSeller.text.toString()
             var description= binding.descriptProductEditSeller.text.toString()
             var price= Integer.parseInt(binding.priceProductEditSeller.text.toString())
@@ -37,26 +54,10 @@ class EditProductDialogFragment : DialogFragment() {
             onProductSellerObserver.editProduct(newproduct)
         }
 
-        binding.cancelChangesButton.setOnClickListener {
+        _binding.cancelChangesButton.setOnClickListener {
+            this.dismiss()
         }
 
-        // Inflate the layout for this fragment
-        return binding.root
-    }
-
-    fun loadData(){
-        var price = Integer.parseInt(product.price.toString())
-        Log.e("Product in Dialog",product.name +" - "+product.description+" - "+product.price )
-        binding.descriptProductEditSeller.setText(product.description)
-        binding.nameProductEditSeller.setText(product.name)
-        //binding.priceProductEditSeller.setText(price)
-    }
-
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val inflater = requireActivity().layoutInflater
-        var dialog = AlertDialog.Builder(requireContext())
-            .setView(inflater.inflate(R.layout.fragment_edit_product_dialog, null))
-            .create()
         return dialog
     }
 
